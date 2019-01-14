cec70dd350eca4ec778e3a3247807692d956df30
Adit Garg (RIT Student) <ag9126@ad.rit.edu>
2019-01-14 08:39:08 -0500

    Week:1 Day:0 Commit:0

---
+++ ZZGarg_Template/sketch.js
@@ -0,0 +1,24 @@
+/**
+ * Adit Garg
+ * IGME-101: Assignment Name, m/d/18
+ * Summarization of sketch activity
+ */
+
+"use strict"; //catch some common coding errors
+
+/**
+ * setup : Initialization runs once; called automatically
+ * Summarize code that you add
+ */
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+}
+
+/**
+ * draw : Periodically called automatically
+ * Summarize code that you add
+ */
+function draw() {
+
+}
