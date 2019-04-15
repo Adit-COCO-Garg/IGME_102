3460e3c52a67e8de795fa59aaba5d978a04443cb
Adit Garg (RIT Student) <ag9126@ad.rit.edu>
2019-04-15 16:21:28 -0400

    P2 UPDATE FNERPWNVP

--- Homework/P2CensusViz/Tract.js
+++ Homework/P2CensusViz/Tract.js
@@ -1,138 +1,80 @@
 class Tract {
-    constructor(idT, x, y) {
-        this.id = idT;
-        this.lat = x;
-        this.long = y;
-        this.population = 0;
-        this.mPop = 0;
-        this.fPop = 0;
-        this.medIncome = 0;
-        this.povertyM = 0;
-        this.povertyF = 0;
-        this.white = 0;
-        this.asian = 0;
-        this.black = 0;
-        this.xPos = 0;
-        this.yPos = 0;
-        this.popMapped = 0;
-        this.mPopMapped = 0;
-        this.fPopMapped = 0;
-        this.medIncomeMapped = 0;
-        this.povertyMMapped = 0;
-        this.povertyFMapped = 0;
-        this.whiteMapped = 0;
-        this.asianMapped = 0;
-        this.blackMapped = 0;
-		Tract.controller1();
-		Tract.controller2();
-		Tract.controller3();
-		Tract.controller4();
-		Tract.controller5();
-		Tract.controller6();
-		
-		
-    }
-	static controller1(){
-		this.control = createSlider(-100, 100);
-        this.control.style("z-index", "100");
-        this.control.size(70);
-        this.control.position(620,120);
-		console.log(this.control.value());
-	}
-	static controller2(){
-		this.control2 = createSlider(-100, 100);
-        this.control2.style("z-index", "100");
-        this.control2.size(70);
-        this.control2.position(620,220);
-		console.log(this.control.value());
-	}
-	static controller3(){
-		this.control3 = createSlider(-100, 100);
-        this.control3.style("z-index", "100");
-        this.control3.size(70);
-        this.control3.position(620,320);
-		console.log(this.control.value());
-	}
-	static controller4(){
-		this.control4 = createSlider(-100, 100);
-        this.control4.style("z-index", "100");
-        this.control4.size(70);
-        this.control4.position(620,420);
-		console.log(this.control.value());
-	}
-	static controller5(){
-		this.control5 = createSlider(-100, 100);
-        this.control5.style("z-index", "100");
-        this.control5.size(70);
-        this.control5.position(620,520);
-		console.log(this.control.value());
-	}
-	static controller6(){
-		this.control6 = createSlider(-100, 100);
-        this.control6.style("z-index", "100");
-        this.control6.size(70);
-        this.control6.position(620,620);
-		console.log(this.control.value());
-	}
-    loadData(tracts) {
-        console.log(tracts)
-        tracts.forEach(fields => {
-            if (!isNaN(fields[0])) {
-                let currTract = fields.split("\t")
-                this.id = currTract[0];
-                this.lat = parseFloat(currTract[1]);
-                this.long = -1 * parseFloat(currTract[2]);
-
-            }
-        })
-    }
-	modifier(){
-		t=new CheckBox();
-	}
-    display() {
-        let INCscale = chroma.scale();
-        let ERGB = INCscale(this.medIncomeMapped);
-        
-        ERGB = ERGB._rgb;
-        fill(ERGB[0], ERGB[1], ERGB[2]);
-        if (!(this.medIncomeMapped < 0)) {
-            ellipse(this.xPos, this.yPos, this.popMapped * 2.5, this.popMapped * 2.5);
-        }
+	constructor(idT, x, y) {
+		this.id = idT;
+		this.lat = x;
+		this.long = y;
+		this.population = 0;
+		this.mPop = 0;
+		this.fPop = 0;
+		this.medIncome = 0;
+		this.povertyM = 0;
+		this.povertyF = 0;
+		this.white = 0;
+		this.asian = 0;
+		this.black = 0;
+		this.xPos = 0;
+		this.yPos = 0;
+		this.popMapped = 0;
+		this.mPopMapped = 0;
+		this.fPopMapped = 0;
+		this.medIncomeMapped = 0;
+		this.povertyMMapped = 0;
+		this.povertyFMapped = 0;
+		this.whiteMapped = 0;
+		this.asianMapped = 0;
+		this.blackMapped = 0;
 
 
-        fill("purple");
-        let Mval = (2 * PI) * this.mPopMapped
-        arc(this.xPos, this.yPos, this.popMapped * 2, this.popMapped * 2, 0, Mval);
-        
-        fill(255, 51, 153);
-        arc(this.xPos, this.yPos, this.popMapped * 2, this.popMapped * 2, Mval, Mval + (2 * PI) * this.fPopMapped);
 
-        //        fill(200,100,100);
-        //        arc(this.xPos, this.yPos, 20, 20, 0, PI);
-        //        arc(this.xPos, this.yPos, 70, 70, PI, PI + QUARTER_PI);
-        //        
-        //arc(this.xPos, this.yPos, 80, 80, PI + QUARTER_PI, TWO_PI);
+	}
 
-        
-        
-        fill("white")
-        arc(this.xPos, this.yPos, this.popMapped*1.50, this.popMapped*1.50, 0, this.whiteMapped);
-        let val=this.whiteMapped
-        
-        fill("yellow")
-        arc(this.xPos, this.yPos, this.popMapped*1.50, this.popMapped*1.50, val, val + this.asianMapped);
+	display() {
+		let INCscale = chroma.scale();
+		let ERGB = INCscale(this.medIncomeMapped);
 
-        fill("black")
-        val +=this.asianMapped
-        arc(this.xPos, this.yPos, this.popMapped*1.50, this.popMapped*1.50, val, val + this.blackMapped);
-        
-        
-        let hueVal = map(this.popMapped, w / 200, w / 180, 0, 1);
-        let CRGB = colorScale(hueVal);
-        CRGB = CRGB._rgb;
-        fill(CRGB[0], CRGB[1], CRGB[2]);
-        ellipse(this.xPos, this.yPos, this.popMapped, this.popMapped);
-    }
+		ERGB = ERGB._rgb;
+		fill(ERGB[0], ERGB[1], ERGB[2]);
+		if (!(this.medIncomeMapped < 0)) {
+			ellipse(this.xPos, this.yPos, this.popMapped * 2.5, this.popMapped * 2.5);
+		}
+
+
+		fill("purple");
+		let Mval = (2 * PI) * this.mPopMapped
+		arc(this.xPos, this.yPos, this.popMapped * 2, this.popMapped * 2, 0, Mval);
+
+		fill(255, 51, 153);
+		arc(this.xPos, this.yPos, this.popMapped * 2, this.popMapped * 2, Mval, Mval + (2 * PI) * this.fPopMapped);
+
+		//        fill(200,100,100);
+		//        arc(this.xPos, this.yPos, 20, 20, 0, PI);
+		//        arc(this.xPos, this.yPos, 70, 70, PI, PI + QUARTER_PI);
+		//        
+		//arc(this.xPos, this.yPos, 80, 80, PI + QUARTER_PI, TWO_PI);
+		let val=0;
+		if (uiControlPop.cb1.checked()) {
+			fill("white")
+			arc(this.xPos, this.yPos, this.popMapped * 1.50, this.popMapped * 1.50, 0, this.whiteMapped);
+			val += this.whiteMapped
+		}
+		if (uiControlPop.cb2.checked()) {
+			fill("yellow")
+			arc(this.xPos, this.yPos, this.popMapped * 1.50, this.popMapped * 1.50, val, val + this.asianMapped);
+			val += this.asianMapped
+		}
+		if (uiControlPop.cb3.checked()) {
+			fill("black")
+			arc(this.xPos, this.yPos, this.popMapped * 1.50, this.popMapped * 1.50, val, val + this.blackMapped);
+			val += this.blackMapped
+		}
+
+
+		let hueVal = map(this.popMapped, w / 200, w / 180, 0, 1);
+		let CRGB = colorScale(hueVal);
+		CRGB = CRGB._rgb;
+		fill(CRGB[0], CRGB[1], CRGB[2]);
+		ellipse(this.xPos, this.yPos, this.popMapped, this.popMapped);
+	}
 
 
 }
--- Homework/P2CensusViz/sketch.js
+++ Homework/P2CensusViz/sketch.js
@@ -7,11 +7,13 @@
 
 //Global variables
 var canvas, tractLocs = [],
-    tractData = [],
-    currRange, currDRange, h, w, tloc, tdat, f, g, colorScale = chroma.scale(['yellow', 'navy']).mode('hsl'),uiControl={};
+	tractData = [],
+	currRange, currDRange, h, w, tloc, tdat, f, g, colorScale = chroma.scale(['yellow', 'navy']).mode('hsl'),
+	uiControlPop = {},
+	uiControlSex = {};
 
 function preload() {
-    loadStrings("2017_gaz_tracts_36.txt", readTracts, readFail)
+	loadStrings("2017_gaz_tracts_36.txt", readTracts, readFail)
 }
 /**
  * readTracts: takes in tracts as a parameter and for each 
@@ -20,58 +22,58 @@ function preload() {
  * tract which accepts fields as parameter
  */
 function readTracts(tracts) {
-    tracts.forEach(fields => {
-        if (!isNaN(fields[0])) {
-            let currTract = fields.split("\t");
-            tractData.push(new Tract(parseInt(currTract[0]), parseFloat(currTract[1]), -1 * (parseFloat(currTract[2]))))
-        }
-    })
-    loadJSON("https://api.census.gov/data/2017/acs/acs5?get=B01003_001E,B01001_002E,B01001_026E,B19013_001E,B17001_003E,B17001_017E,B02001_002E,B02001_005E,B02001_003E,NAME&for=tract:*&in=state:36&in=county:055", parseJSON, readFail)
+	tracts.forEach((fields) => {
+		if (!isNaN(fields[0])) {
+			let currTract = fields.split("\t");
+			tractData.push(new Tract(parseInt(currTract[0]), parseFloat(currTract[1]), -1 * (parseFloat(currTract[2]))))
+		}
+	})
+	loadJSON("JSONdata.JSON", parseJSON, readFail);
 
 
 }
 
 function parseJSON(ln) {
-    console.log(ln)
-    
-    ln.forEach(line => {
-        console.log(line[0])
-        if (!isNaN(line[0])) {
-            let r = tractData.find(tract => tract.id == line[line.length - 1])
-            console.log(r)
-            if (r != undefined) {
-                if ((r.id != 980000) || (r.id != 990000)) {
-                    r.population = line[0];
-                    r.mPop = line[1];
-                    r.fPop = line[2];
-                    r.medIncome = line[3];
-                    r.povertyM = line[4];
-                    r.povertyF = line[5];
-                    r.white = line[6];
-                    r.asian = line[7];
-                    r.black = line[8];
-                }
+	console.log(ln)
 
-            }
+	ln.forEach(line => {
+		console.log(line[0])
+		if (!isNaN(line[0])) {
+			let r = tractData.find(tract => tract.id == line[line.length - 1])
+			console.log(r)
+			if (r != undefined) {
+				if ((r.id != 980000) || (r.id != 990000)) {
+					r.population = line[0];
+					r.mPop = line[1];
+					r.fPop = line[2];
+					r.medIncome = line[3];
+					r.povertyM = line[4];
+					r.povertyF = line[5];
+					r.white = line[6];
+					r.asian = line[7];
+					r.black = line[8];
+				}
 
-        }
-    })
-    console.log(tractData)
+			}
+
+		}
+	})
+	console.log(tractData)
 }
 //    ln.forEach(l=>tractData.find(finder))
 
 
 function finder(t) {
-    console.log(t)
-    if (t.id == l[line.length - 1]) {
-        console.log("yolo")
-        t.population = line[0];
-        t.mPop = line[1];
-        t.fPop = line[2];
-        t.medIncome = line[3];
-        t.povertyM = line[4];
-        t.povertyF = line[5];
-    }
+	console.log(t)
+	if (t.id == l[line.length - 1]) {
+		console.log("yolo")
+		t.population = line[0];
+		t.mPop = line[1];
+		t.fPop = line[2];
+		t.medIncome = line[3];
+		t.povertyM = line[4];
+		t.povertyF = line[5];
+	}
 }
 
 
@@ -81,57 +83,88 @@ function finder(t) {
  * Summarize code that you add
  */
 function setup() {
+
+	canvasCreat(); //canvas creator function called
 	setupUiControl();
-    canvasCreat(); //canvas creator function called
-    background(195, 2, 83); //Light Gray Background
-    currRange = mapper(tractData);
-    currDRange = Smapper(tractData);
-    chroma.scale(['yellow', 'navy']).mode('hsl');
-    dataMapper();
-    background(207, 211, 212); //Light Gray Background
-    tractData.forEach(t => t.display())
+	background(195, 2, 83); //Light Gray Background
+	currRange = mapper(tractData);
+	currDRange = Smapper(tractData);
+	chroma.scale(['yellow', 'navy']).mode('hsl');
+	dataMapper();
+	background(207, 211, 212); //Light Gray Background
+	tractData.forEach(t => t.display())
 
 }
 
-function setupUiControl(){
-	
-	uiControl.cb1 = createCheckbox("Race: ",true);
-	uiControl.cb1.style("z-index", "100")
-	uiControl.cb1.position(620,920)
-	
+function setupUiControl() {
+	//pop raace
+	uiControlPop.cb1 = createCheckbox("Race: White", true);
+	uiControlPop.cb1.style("z-index", "100")
+	console.log("hi", w)
+	uiControlPop.cb1.position(w * 0.89, h * 0.01)
+	uiControlPop.cb2 = createCheckbox("Race: Asian", true);
+	uiControlPop.cb2.style("z-index", "100")
+	uiControlPop.cb2.position(w * 0.89, h * 0.03)
+	uiControlPop.cb3 = createCheckbox("Race: Black ", true);
+	uiControlPop.cb3.style("z-index", "100")
+	uiControlPop.cb3.position(w * 0.89, h * 0.05)
+	uiControlPop.cb4 = createCheckbox("Race: All", true);
+	uiControlPop.cb4.style("z-index", "100")
+	uiControlPop.cb4.position(w * 0.89, h * 0.07)
+	uiControlPop.cb1.changed(dataMapper)
+	uiControlPop.cb2.changed(dataMapper)
+	uiControlPop.cb3.changed(dataMapper)
+	uiControlPop.cb4.changed(dataMapper)
+
+	//male fem
+	uiControlSex.cb1 = createCheckbox("Sex: Male", true);
+	uiControlSex.cb1.style("z-index", "100")
+	uiControlSex.cb1.position(w * 0.89, h * 0.15)
+	uiControlSex.cb2 = createCheckbox("Sex: Female", true);
+	uiControlSex.cb2.style("z-index", "100")
+	uiControlSex.cb2.position(w * 0.89, h * 0.17)
+	uiControlSex.cb3 = createCheckbox("Sex: All", true);
+	uiControlSex.cb3.style("z-index", "100")
+	uiControlSex.cb3.position(w * 0.89, h * 0.19)
+
+
+
 }
+
 function mapper(tractD) {
-    let tx = [],
-        ty = []
-    tractD.forEach(t => {
-        tx.push(t.lat);
-        ty.push(t.long);
-    })
-    return [min(tx), max(tx), min(ty), max(ty)]
+	let tx = [],
+		ty = []
+	tractD.forEach(t => {
+		tx.push(t.lat);
+		ty.push(t.long);
+	})
+	return [min(tx), max(tx), min(ty), max(ty)]
 }
 
 function Smapper(tract) {
-    let t1 = [],
-        t2 = [],
-        t3 = [],
-        t4 = [],
-        t5 = [],
-        t6 = [],
-		t7 = [],t8 = [],t9 = [];
-    tract.forEach(t => {
-        if (t.medIncome>0) {
-            t1.push(t.population);
-            t2.push(t.mPop);
-            t3.push(t.fPop);
-            t4.push(t.medIncome);
-            t5.push(t.povertyM);
-            t6.push(t.povertyF);
-            t7.push(t.white)
-            t8.push(t.asian)
-            t9.push(t.black)
-        }
-    })
-    return [min(t1), max(t1), min(t2), max(t2), min(t3), max(t3), min(t4), max(t4), min(t5), max(t5), min(t6), max(t6),min(t7),max(t7),min(t8),max(t8),min(t9),max(t9)]
+	let t1 = [],
+		t2 = [],
+		t3 = [],
+		t4 = [],
+		t5 = [],
+		t6 = [],
+		t7 = [],
+		t8 = [],
+		t9 = [];
+	tract.forEach(t => {
+		if (t.medIncome > 0) {
+			t1.push(t.population);
+			t2.push(t.mPop);
+			t3.push(t.fPop);
+			t4.push(t.medIncome);
+			t5.push(t.povertyM);
+			t6.push(t.povertyF);
+			t7.push(t.white)
+			t8.push(t.asian)
+			t9.push(t.black)
+		}
+	})
+	return [min(t1), max(t1), min(t2), max(t2), min(t3), max(t3), min(t4), max(t4), min(t5), max(t5), min(t6), max(t6), min(t7), max(t7), min(t8), max(t8), min(t9), max(t9)]
 }
 
 /**
@@ -140,14 +173,14 @@ function Smapper(tract) {
  * reformating them based on datatypes created before
  */
 function printTract(fields) {
-    //	let count = new Intl.NumberFormat('en-US');
-    //	let percent = new Intl.NumberFormat('en-US', {
-    //		style: 'percent'
-    //	});
-    //	let dollars = new Intl.NumberFormat('en-US', {
-    //		style: 'currency',currency:"USD"
-    //	});
-    console.table(fields);
+	//	let count = new Intl.NumberFormat('en-US');
+	//	let percent = new Intl.NumberFormat('en-US', {
+	//		style: 'percent'
+	//	});
+	//	let dollars = new Intl.NumberFormat('en-US', {
+	//		style: 'currency',currency:"USD"
+	//	});
+	console.table(fields);
 
 }
 /**
@@ -165,53 +198,85 @@ function draw() {
  * creates a canvas and sets some default values
  */
 function canvasCreat() {
-    //Canvas setup
-    h = innerHeight;
-    w = innerWidth;
-    canvas = createCanvas(w, h); // defining the canvas as a variable allows it to be versatile
-    canvas.position(0, 0); //due to the variable's versatility the DOM library allows to define position
-    canvas.style("z-index", "-1"); //due to the variable's versatility the DOM library allows to define the hirearchy of view/ z index (a css property)
-    colorMode(RGB, 255, 255, 255); //color mode set to HSB
-    //--------
-    noStroke(); //No stroke
+	//Canvas setup
+	h = innerHeight;
+	w = innerWidth;
+	canvas = createCanvas(w, h); // defining the canvas as a variable allows it to be versatile
+	canvas.position(0, 0); //due to the variable's versatility the DOM library allows to define position
+	canvas.style("z-index", "-1"); //due to the variable's versatility the DOM library allows to define the hirearchy of view/ z index (a css property)
+	colorMode(RGB, 255, 255, 255); //color mode set to HSB
+	//--------
+	noStroke(); //No stroke
 }
 
 function windowResized() {
-    h = innerHeight
-    w = innerWidth
-    canvas = resizeCanvas(innerWidth, innerHeight);
-    dataMapper();
-    background(207, 211, 212); //Light Gray Background
-    tractData.forEach(t => t.display())
+	h = innerHeight
+	w = innerWidth
+	canvas = resizeCanvas(innerWidth, innerHeight);
+	dataMapper();
+	background(207, 211, 212); //Light Gray Background
+	tractData.forEach(t => t.display())
 }
 
 function dataMapper() {
-    tractData.forEach(t => {
-        t.xPos = map(t.lat, currRange[0], currRange[1], w / 20, w - (w / 20));
-        t.yPos = map(t.long, currRange[2], currRange[3], h / 20, h - (h / 20));
-        t.popMapped = map(t.population, currDRange[0], currDRange[1], w / 200, w / 180); //color
-        t.mPopMapped = (t.mPop) / t.population; //arcs
-        t.fPopMapped = (t.fPop) / t.population; //arcs
-        t.medIncomeMapped = map(t.medIncome, currDRange[6], currDRange[7], 0, 1); //hover?
-        t.povertyMMapped = map(t.povertyM, currDRange[8], currDRange[9], 200, 300); //sub arcs?
-        t.povertyFMapped = map(t.povertyF, currDRange[10], currDRange[11], 200, 300); //sub arcs?
-        let tot=parseInt(t.white)+parseInt(t.asian)+parseInt(t.black)
-        console.log("total",tot)
-        console.log("w",t.white)
-        console.log("b",t.black)
-        console.log("a",t.asian)
-        if (tot!=0){
-            t.whiteMapped = (t.white/tot)*2*PI
-            t.asianMapped = (t.asian/tot)*2*PI
-            t.blackMapped = (t.black/tot)*2*PI
-        }
-        
+	tractData.forEach(t => {
+		t.xPos = map(t.lat, currRange[0], currRange[1], w / 20, w - (w / 20));
+		t.yPos = map(t.long, currRange[2], currRange[3], h / 20, h - (h / 20));
+		t.popMapped = map(t.population, currDRange[0], currDRange[1], w / 200, w / 180); //color
+		t.mPopMapped = (t.mPop) / t.population; //arcs
+		t.fPopMapped = (t.fPop) / t.population; //arcs
+		t.medIncomeMapped = map(t.medIncome, currDRange[6], currDRange[7], 0, 1); //hover?
+		t.povertyMMapped = map(t.povertyM, currDRange[8], currDRange[9], 200, 300); //sub arcs?
+		t.povertyFMapped = map(t.povertyF, currDRange[10], currDRange[11], 200, 300); //sub arcs?
+		let tot = 0;
+		let counter = 0;
+		if (uiControlPop.cb4.checked()) {
+			uiControlPop.cb4.checked(true);
+			counter=3;
+		} 
+		if (uiControlPop.cb1.checked()) {
+			console.log("WHITE");
+			tot += parseInt(t.white);
+			counter++;
+		} 
+		if (uiControlPop.cb2.checked()) {
+			console.log("ASIAN");
+			tot += parseInt(t.asian);
+			counter++;
+		} 
+		if (uiControlPop.cb3.checked()) {
+			console.log("BLACK");
+			tot += parseInt(t.black);
+			counter++;
+		} 
+		if (counter<3) {
+			uiControlPop.cb4.checked(false);
+		}
+		if (counter>=3) {
+			uiControlPop.cb4.checked(true);
+			uiControlPop.cb1.checked(true)
+			uiControlPop.cb2.checked(true)
+			uiControlPop.cb3.checked(true)
+			tot=parseInt(t.black)+parseInt(t.asian)+parseInt(t.white);
+			
+		}
+		console.log(tot);
 
-    })
+		console.log("w", t.white)
+		console.log("b", t.black)
+		console.log("a", t.asian)
+		if (tot != 0) {
+			t.whiteMapped = (t.white / tot) * 2 * PI
+			t.asianMapped = (t.asian / tot) * 2 * PI
+			t.blackMapped = (t.black / tot) * 2 * PI
+		}
+
+
+	})
 }
 /**
  * readFail: spits error message when relevant
  */
 function readFail() {
-    console.log("Read failed! Try again, sorry for the inconvenience");
+	console.log("Read failed! Try again, sorry for the inconvenience");
 }
