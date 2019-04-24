0598e37b392cf38a81003d2f45d019daebe29119
Adit-COCO-Garg <adit.garg21k@gmail.com>
2019-04-24 14:00:12 -0400

    FRACTAL TREES

--- Week 14/ZZGarg_Template/sketch.js
+++ Week 14/ZZGarg_Template/sketch.js
@@ -6,16 +6,22 @@
 "use strict"; //catch some common coding errors
 
 //Global variables
-var canvas;
+var canvas,branch1;
 
 /**
  * setup : Initialization runs once; called automatically
  * Summarize code that you add
  */
 function setup() {
-	canvasCreat(); //canvas creator function called
+//	strokeCap(SQUARE);
+	canvasCreat();
 	background(195, 2, 83); //Light Gray Background
+	angleMode(DEGREES)
+	let root = createVector(width / 2, height);
+	branch1 = new branch(root,-90,100);
+	branch1.display();
 }
+
 
 /**
  * draw : Periodically called automatically
@@ -28,12 +34,13 @@ function draw() {
  * canvasCreat : Canvas creator code and defaults
  * creates a canvas and sets some default values
  */
-function canvasCreat(){
+function canvasCreat() {
 	//Canvas setup
-    canvas = createCanvas(1000, 1000); // defining the canvas as a variable allows it to be versatile
-    canvas.position(0, 0); //due to the variable's versatility the DOM library allows to define position
-    canvas.style("z-index", "-1"); //due to the variable's versatility the DOM library allows to define the hirearchy of view/ z index (a css property)
-    colorMode(HSB, 360, 100, 100); //color mode set to HSB
-    //--------
-	noStroke(); //No stroke
+	
+	canvas = createCanvas(600, 600); // defining the canvas as a variable allows it to be versatile
+	canvas.position(0, 0); //due to the variable's versatility the DOM library allows to define position
+	canvas.style("z-index", "-1"); //due to the variable's versatility the DOM library allows to define the hirearchy of view/ z index (a css property)
+	colorMode(HSB, 360, 100, 100); //color mode set to HSB
+	//--------
+//	noStroke(); //No stroke
 }
